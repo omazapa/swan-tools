4a8276539f50f5a31e591871bd2f0d44bf729524
Omar Zapata <Omar.Zapata@cern.ch>
2019-09-17 11:43:39 +0200

    basic script to debug swan outputs

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # swan-tools
 Some tools  for the Rota in SWAN
+
+# usege
+* init your kerberos session with kinit user@CERN.CH
+* source swan_env.sh
+
+Run the next command to  get container by node, use grep to filter users or the needed information
+```.sh
+swan_docker_ps
+```
+
+```.sh
+
+To get the journalctl output run
+```.sh
+swan_journalctl 6 2f01da4ea995
+```
+
+Where 6 is the swan node 6 and 2f01da4ea995 is the container id 
+
---
+++ swan_env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# Function to get the lists of containers in swan nodes
+# You can to run grep to the output to filter user for example.
+function swan_docker_ps()
+{ 
+	for i in $(seq 1 1 6);
+	do 
+		echo "==============================" SWAN NODE swan00$i "=============================="
+		ssh root@swan00$i docker ps
+	done
+}
+
+# Function to get the container output using journalctl
+# parameters 
+# $1 : swan node
+# $2 : container id 
+function swan_journalctl()
+{
+	ssh root@swan00$1 journalctl CONTAINER_ID=$2
+}
+
+# Function to connect to a container in a swan node
+# parameters 
+# $1 : swan node
+# $2 : container id 
+function swan_container_exec()
+{
+	ssh -t root@swan00$1 docker exec -it $2 /bin/bash
+}
